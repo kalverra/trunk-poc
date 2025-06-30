072e3ff62b0907e9f252fa09374638a7c23f5642
Adam Hamrick <adam.hamrick@smartcontract.com>
2025-06-30 17:43:48 -0400

    branch-out quarantine tests
    example_tests/fail/fail_test.go: TestFail
    example_tests/subtests/subtests_test.go: TestFail

--- example_tests/fail/fail_test.go
+++ example_tests/fail/fail_test.go
@@ -1,10 +1,16 @@
 package fail
 
 import (
+	"os"
 	"testing"
 )
 
 func TestFail(t *testing.T) {
+	if os.Getenv("RUN_QUARANTINED_TESTS") != "true" {
+		t.Skip("Flaky test quarantined. Ticket <Jira ticket>. Done automatically by branch-out (https://github.com/smartcontractkit/branch-out)")
+	} else {
+		t.Logf("'RUN_QUARANTINED_TESTS' set to '%s', running quarantined test", os.Getenv("RUN_QUARANTINED_TESTS"))
+	}
 	t.Parallel()
 
 	t.Log("I fail")
--- example_tests/subtests/subtests_test.go
+++ example_tests/subtests/subtests_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"os"
 )
 
 func TestPass(t *testing.T) {
@@ -22,6 +23,11 @@ func TestPass(t *testing.T) {
 }
 
 func TestFail(t *testing.T) {
+	if os.Getenv("RUN_QUARANTINED_TESTS") != "true" {
+		t.Skip("Flaky test quarantined. Ticket <Jira ticket>. Done automatically by branch-out (https://github.com/smartcontractkit/branch-out)")
+	} else {
+		t.Logf("'RUN_QUARANTINED_TESTS' set to '%s', running quarantined test", os.Getenv("RUN_QUARANTINED_TESTS"))
+	}
 	t.Parallel()
 
 	t.Run("failing subtest", func(t *testing.T) {
